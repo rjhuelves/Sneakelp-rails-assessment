5e0b9534fcf63cbc5409b24e8778d33a56edb840
Richard Huelves <rhuelves@gmail.com>
2020-01-28 12:37:44 -0500

    associate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :reviews
+    has_many :reviewed_sneakers, through: :reviews, source: :sneaker
+
+    has_many :sneakers #sneaker that user has created 
 end
