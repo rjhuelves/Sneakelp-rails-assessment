9002630e9b8d8c8aae4e333b8e66273f709765b2
Richard Huelves <rhuelves@gmail.com>
2020-01-28 14:14:58 -0500

    users_controller create and show. user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_sneakers, through: :reviews, source: :sneaker
-
     has_many :sneakers #sneaker that user has created 
+
+    validates :username, :email, uniqueness: true, pressence: true 
+    has_secure_password 
 end
